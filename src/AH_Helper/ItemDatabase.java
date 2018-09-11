86d8423ee2e01e98637fb8ac2fca55de12b0b410
Evohunt <evohuntmaster@gmail.com>
2018-09-11 18:13:36 +0300

    Home + Database GUI

---
+++ src/AH_Helper/ItemDatabase.java
@@ -0,0 +1,100 @@
+package AH_Helper;
+
+import java.io.Serializable;
+
+public class ItemDatabase implements Serializable {
+
+    private int starlight_rose_price;
+    private int foxflower_price;
+    private int aethril_price;
+    private int fjarnskaggl_price;
+    private int dreamleaf_price;
+
+    private int countless_armies_price;
+    private int ten_thousand_scars_price;
+    private int seventh_demon_price;
+    private int whispered_pact_price;
+
+    private int obliterum_price;
+
+
+    public int getDreamleaf_price() {
+        return dreamleaf_price;
+    }
+
+    public void setDreamleaf_price(int dreamleaf_price) {
+        this.dreamleaf_price = dreamleaf_price;
+    }
+
+    public int getObliterum_price() {
+        return obliterum_price;
+    }
+
+    public void setObliterum_price(int obliterum_price) {
+        this.obliterum_price = obliterum_price;
+    }
+
+    public int getStarlight_rose_price() {
+        return starlight_rose_price;
+    }
+
+    public void setStarlight_rose_price(int starlight_rose_price) {
+        this.starlight_rose_price = starlight_rose_price;
+    }
+
+    public int getFoxflower_price() {
+        return foxflower_price;
+    }
+
+    public void setFoxflower_price(int foxflower_price) {
+        this.foxflower_price = foxflower_price;
+    }
+
+    public int getAethril_price() {
+        return aethril_price;
+    }
+
+    public void setAethril_price(int aethril_price) {
+        this.aethril_price = aethril_price;
+    }
+
+    public int getFjarnskaggl_price() {
+        return fjarnskaggl_price;
+    }
+
+    public void setFjarnskaggl_price(int fjarnskaggl_price) {
+        this.fjarnskaggl_price = fjarnskaggl_price;
+    }
+
+    public int getCountless_armies_price() {
+        return countless_armies_price;
+    }
+
+    public void setCountless_armies_price(int countless_armies_price) {
+        this.countless_armies_price = countless_armies_price;
+    }
+
+    public int getTen_thousand_scars_price() {
+        return ten_thousand_scars_price;
+    }
+
+    public void setTen_thousand_scars_price(int ten_thousand_scars_price) {
+        this.ten_thousand_scars_price = ten_thousand_scars_price;
+    }
+
+    public int getSeventh_demon_price() {
+        return seventh_demon_price;
+    }
+
+    public void setSeventh_demon_price(int seventh_demon_price) {
+        this.seventh_demon_price = seventh_demon_price;
+    }
+
+    public int getWhispered_pact_price() {
+        return whispered_pact_price;
+    }
+
+    public void setWhispered_pact_price(int whispered_pact_price) {
+        this.whispered_pact_price = whispered_pact_price;
+    }
+}
--- src/AH_Helper/MainController.java
+++ src/AH_Helper/MainController.java
@@ -1,4 +1,28 @@
 package AH_Helper;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class MainController {
+    public TextField starlight_rose_price;
+    public TextField aethril_price;
+    public TextField foxflower_price;
+    public TextField fjarnskaggl_price;
+    public TextField countless_armies_price;
+    public TextField whispered_pact_price;
+    public TextField seventh_demon_price;
+    public TextField ten_thousand_scars_price;
+    public TextField obliterum_price;
+    public TextField dreamleaf_price;
+    public Button btn_update_database;
+    public Button btn_check_database;
+
+    public void UpdateDatabase(ActionEvent actionEvent) {
+
+    }
+
+    public void CheckDatabase(ActionEvent actionEvent) {
+
+    }
 }
